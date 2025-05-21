fa3d1dfdbab51a333856943e384ff7f762118193
YanamiYokico <v.a.nikolojchyk@gmail.com>
2025-05-21 17:52:14 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export const App: React.FC = () => {
+  useEffect(() => {
+    const handleKeyboardKey = (event: KeyboardEvent) => {
+      const key = event.key;
+
+      document.querySelector('.App__message')!.textContent =
+        `The last pressed key is [${key}]`;
+    };
+
+    window.addEventListener('keydown', handleKeyboardKey);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyboardKey);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <p className="App__message">Nothing was pressed yet</p>
+    </div>
+  );
+};
